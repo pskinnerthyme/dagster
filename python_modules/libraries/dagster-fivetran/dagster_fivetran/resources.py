0521c7fd11568d5e7ac8748270b56e702a683040
Maxime Armstrong <maxime@retain.ai>
2024-11-15 09:48:47 -0500

    Hash tuple

--- python_modules/libraries/dagster-fivetran/dagster_fivetran/resources.py
+++ python_modules/libraries/dagster-fivetran/dagster_fivetran/resources.py
@@ -904,7 +904,7 @@ class FivetranWorkspace(ConfigurableResource):
         )
 
     def __hash__(self):
-        return hash(self.account_id + self.api_key + self.api_secret)
+        return hash((self.account_id + self.api_key + self.api_secret))
 
 
 @lru_cache(maxsize=None)
